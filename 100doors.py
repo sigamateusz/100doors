d399f18f26a61c2070273ccd11cdf03c86e0600a
Mateusz Siga <sigamateusz@gmail.com>
2016-10-31 13:38:52 +0100

    In 12 lines

--- 100doors.py
+++ 100doors.py
@@ -1,56 +1,12 @@
 import os
 os.system('clear') # clear screen
 
-def hundred_closed_doors():
-    """Creating a hundred closed doors"""
-    global doors
-    for i in range(100):
-        doors.append("close")
+doors=[False] * 100
 
+for x in range(1,101):
+    for i in range(x-1,100, x):
+        doors[i] = not doors[i]
 
-def toggle_the_door(n):
-    """
-    If the door is closed it opens
-    If the door is opened it closes
-    """
-    global doors
-    if doors[n]=="close":
-        return "open"
-    if doors[n]=="open":
-        return "close"
-
-
-def show_all_doors():
-    """This function can show all doors"""
-    for i in range(100):
-        print("%3d." %(i+1), end=" ")
-        print(doors[i])
-
-
-def toggle_range(n):
-    """Changes the status of all doors"""
-    global doors
-    for i in range(n-1,100,n):
-        doors[i]=toggle_the_door(i)
-
-
-def open_doors():
-    """Prints all open doors """
-    global doors
-    print("The following doors are open: ")
-    for i in range(100):
-        if doors[i]=="open":
-            print("%3d." %(i+1), end=" ")
-            print(doors[i])
-
-doors=[]
-
-def main():
-    hundred_closed_doors()
-    for i in range(1,101):
-        toggle_range(i)
-    open_doors()
-
-
-if __name__=="__main__":
-    main()
+for i in range(100):
+    if doors[i]==True:
+        print("%3d.  Otwarte" %(i+1))
